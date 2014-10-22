39aef086225d3bf50461500ad4193dd21abec1a5
Ivan Scherbak <dev@funivan.com>
2014-10-22 17:59:11 +0300

    rewrite gnome::desktop_program

--- manifests/desktop_program.pp
+++ manifests/desktop_program.pp
@@ -7,18 +7,24 @@ define gnome::desktop_program(
   $user = $::gnome::params::user
   $homeDir = "/home/$user"
 
-  file { ["/usr/local/bin/$program" ]:
+  $applicationDir = "$homeDir/.local/share/applications"
+
+
+  file { "/usr/local/bin/$program":
     ensure => link,
     target => "$path",
   }
-  ->
-  file { ["$homeDir/.local/", "$homeDir/.local/share", "$homeDir/.local/share/applications"] :
-    ensure => directory,
-    owner  => $::gnome::params::user,
-  } -> gnome::desktop_entry { "$homeDir/.local/share/applications/$program.desktop" :
-    exec        => "$program",
-    entryName   => "$program",
-    icon        => "$icon",
+
+  -> exec { "create dir for $program " :
+    command => "mkdir -p $applicationDir",
+    user    => $user,
+    onlyif  => ["test -d $applicationDir"],
+  }
+
+  -> gnome::desktop_entry { "$applicationDir/$program.desktop" :
+    exec        => $program,
+    entryName   => $program,
+    icon        => $icon,
   }
 
 }
