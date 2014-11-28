9c6126faf26371e10557feca3104f3a3320343a5
Ivan Scherbak <dev@funivan.com>
2014-11-28 17:23:04 +0200

    remove reset keys. Clean command

--- manifests/gsettings/command.pp
+++ manifests/gsettings/command.pp
@@ -6,17 +6,7 @@ define gnome::gsettings::command(
 	validate_bool($::gnome::params::logoutput)
 	
   exec { $command:
-    command   => "
-				PID=$(pgrep gnome-session);
-				echo \$PID;
-				export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/\$PID/environ|cut -d= -f2-);
-				echo \$DBUS_SESSION_BUS_ADDRESS;
-				export DISPLAY=0:0;
-				export GSETTINGS_BACKEND=dconf;
-				echo \$GSETTINGS_BACKEND;
-				
-				$command
-			",
+    command   => "$command",
     user      => $::gnome::params::user,
     logoutput => $::gnome::params::logoutput
   }
